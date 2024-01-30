8a44e1de2c7f08aba6273680fc87610c8ec9b4f4
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-30 10:34:57 -0300

    product list section

--- fashion-store/src/app/components/sections/product-list-section/product-list-section.component.ts
+++ fashion-store/src/app/components/sections/product-list-section/product-list-section.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IProduct } from '../../../interfaces/product.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list-section.component.html',
   styleUrl: './product-list-section.component.scss'
 })
 export class ProductListSectionComponent {
-
+  @Input() title!: string;
+  @Input() productList!: IProduct[];
 }
