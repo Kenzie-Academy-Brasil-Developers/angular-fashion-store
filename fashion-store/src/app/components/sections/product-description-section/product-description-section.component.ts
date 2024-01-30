6e26cad63fb89eaa0b78b540fb94be0d4521bf31
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-30 10:14:54 -0300

    providing requests in components

--- fashion-store/src/app/components/sections/product-description-section/product-description-section.component.ts
+++ fashion-store/src/app/components/sections/product-description-section/product-description-section.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductRequest } from '../../../api/product.request';
+import { IProduct } from '../../../interfaces/product.interface';
 
 @Component({
   selector: 'app-product-description-section',
   standalone: true,
   imports: [],
   templateUrl: './product-description-section.component.html',
-  styleUrl: './product-description-section.component.scss'
+  styleUrl: './product-description-section.component.scss',
 })
 export class ProductDescriptionSectionComponent {
+  readonly productSignal = signal<IProduct | null>(null);
 
+  constructor(
+    private route: ActivatedRoute,
+    private productRequest: ProductRequest
+  ) {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get("id");
+
+      if(id){
+        this.productRequest.getProduct(id).subscribe(data => {
+          this.productSignal.set(data);
+        })
+      }
+    })
+  }
+
+  get product(){
+    return this.productSignal();
+  }
 }
--- fashion-store/src/app/pages/product-page/product-page.component.ts
+++ fashion-store/src/app/pages/product-page/product-page.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ProductRequest } from '../../api/product.request';
+import { IProduct } from '../../interfaces/product.interface';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
   imports: [],
+  providers: [ProductRequest],
   templateUrl: './product-page.component.html',
-  styleUrl: './product-page.component.scss'
+  styleUrl: './product-page.component.scss',
 })
 export class ProductPageComponent {
+  readonly productListSignal = signal<IProduct[]>([]);
 
+  constructor(
+    private route: ActivatedRoute,
+    private productRequest: ProductRequest
+  ) {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+
+      if (id) {
+        this.productRequest.getProductList().subscribe((data) => {
+          const newData = data.filter((product) => product.id !== Number(id));
+          this.productListSignal.set(newData);
+        });
+      }
+    });
+  }
+
+  get productList() {
+    return this.productListSignal();
+  }
 }
