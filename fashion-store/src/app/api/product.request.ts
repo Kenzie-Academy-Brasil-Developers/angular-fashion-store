8c6d2be0019bf030192c11ed13b029efeee4c121
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-30 09:30:32 -0300

    http client + product request

---
+++ fashion-store/src/app/api/product.request.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IProduct } from '../interfaces/product.interface';
+
+@Injectable({ providedIn: 'root' })
+export class ProductRequest {
+  private BASE_URL = 'http://localhost:3001';
+
+  constructor(private http: HttpClient) {}
+
+  getProductList() {
+    return this.http.get<IProduct[]>(`${this.BASE_URL}/products`);
+  }
+
+  getProduct(productId: string) {
+    return this.http.get<IProduct>(`${this.BASE_URL}/procucts/${productId}`);
+  }
+}
